3d2d2e6eccb5c6bda5d7d71c0827a3f90a61df66
Diego Jefferson <diegojfsr@gmail.com>
2024-02-20 19:43:48 -0300

    4_ParticularidadesJavaScript

---
+++ 3_ParticularidadesJavaScript/js/scripts.js
@@ -0,0 +1,14 @@
+
+// Execução de cima para baixo
+console.log("Primeiro")
+console.log("Último")
+
+// Case-sensitive
+console.log(" Mais um console.log ")
+
+// Fracamente tipada
+var a = 1;
+
+a = 'teste';
+
+console.log(a);
