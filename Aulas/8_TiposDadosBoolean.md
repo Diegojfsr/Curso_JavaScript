7ea0ab449d28f046164d286fa618f631973c266f
Diego Jefferson <diegojfsr@gmail.com>
2024-02-21 17:46:24 -0300

    8_TiposDadosBoolean

---
+++ Aulas/8_TiposDadosBoolean.md
@@ -0,0 +1,14 @@
+
+Tipos de dados: Boolean.
+
+- O Boolean representa um valor verdadeiro ou falso.
+- Comparações resultam em booleans.
+- Podemos também atribuir um true ou false para uma variável e ela terá o tipo Boolean.
+
+
+
+
+
+
+
+
---
+++ Exercicios/7_TipoDadosBoolean/js/scripts.js
@@ -0,0 +1,17 @@
+
+// Tipos de dados do tipo Boolean.
+
+
+var verdadeiro = true;
+console.log(verdadeiro);
+console.log(typeof verdadeiro);
+
+
+var falso = false;
+console.log(falso);
+console.log(typeof falso);
+
+
+
+console.log(typeof true);
+console.log(typeof false);
