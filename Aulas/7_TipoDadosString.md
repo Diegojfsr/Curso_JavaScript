27346260fe6f437db481c2741dd0a988186b38ca
Diego Jefferson <diegojfsr@gmail.com>
2024-02-21 17:26:34 -0300

    7_TipoDadosString

---
+++ Aulas/7_TipoDadosString.md
@@ -0,0 +1,11 @@
+
+Tipos de dados: String
+
+- String é um tipo de dado para texto.
+- As strings podem ser ecritas com aspas simples e duplas.
+- Um número entre aspas pode ser considerados uma string.
+- É possível também concatenar strins com o +.
+- Vamos ver isso na prática!
+
+
+
---
+++ Exercicios/6_TipoDadosString/js/scripts.js
@@ -0,0 +1,37 @@
+
+// Tipos de dados do tipo String
+
+
+var nome = "Mateus";
+var sobrenome = "Battisti";
+console.log(nome);
+console.log(typeof nome);
+
+var nomeCompleto = nome + " " + sobrenome;
+console.log(nomeCompleto);
+
+var numeroString = "5.2929";
+console.log(numeroString);
+console.log(typeof numeroString);
+
+var frase = 'Esta é uma frase complexa.';//aspas simples tambem funciona
+console.log(frase);
+console.log(typeof frase);
+
+console.log(nome + " " + frase);
+
+
+//document.write serve para escrever mensagens direto no html(navegador, para o usuário.)
+document.write("Olá"); 
+
+// Escrever textocom aspas, e so envolver o texto entre aspas simples. 
+// ou o cantrario. duplas/simples | simples/duplas
+document.write('Ele disse "Ola!"');
+
+// Concatenando Numero e letra.
+console.log( "Este numero: " + "432" );
+
+// Concatenando Variavel e letra.
+console.log( "Este nome: " + nome );
+
+
