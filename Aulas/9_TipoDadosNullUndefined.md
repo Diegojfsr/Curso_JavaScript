44f5f96b44ef35f205ca3d3de2383d3c020fbbd5
Diego Jefferson <diegojfsr@gmail.com>
2024-02-21 18:30:38 -0300

    9_TipoDadosNullUndefined

---
+++ Aulas/9_TipoDadosNullUndefined.md
@@ -0,0 +1,14 @@
+
+Tipos de dados: null e undefined
+
+
+- undefined e null tambem sao considerados tipos d edados.
+- O null é um tipo de dados que representa um valor.
+- O undefined é um tipo de dado para uma variável com valor não atribuido.
+- Vamos ver na prática!
+
+
+
+
+
+
---
+++ Exercicios/8_TipoDadosNullUndefined/js/scripts.js
@@ -0,0 +1,17 @@
+
+// Tipos de dados do tipo null e undefined.
+
+// Hoisting - Içamento - todas as variaveis sao levada para o topo do codigo antes do processamento.
+console.log(sobrenome);
+
+
+var nome = null; // var tipo null
+var sobrenome; // var tipo undefined
+
+console.log(nome);
+console.log(sobrenome);
+
+nome = "Mateus";
+sobrenome = "Battisti";
+console.log(nome);
+console.log(sobrenome);
