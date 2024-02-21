781e44c760144e44d1dec9edbf9d173aa12b25a4
Diego Jefferson <diegojfsr@gmail.com>
2024-02-21 16:12:47 -0300

    4_Variaveis

---
+++ Aulas/5_Variaveis.md
@@ -0,0 +1,6 @@
+Variaveis
+
+- As variaveis armazenam valores.
+- Estes valores podem ser utilizados posteriormente.
+- No JavaScript é Possível criar variaveis de 3 formas.
+- Podemos mudar o tipo da variável livrimente(tipagem fraca)
---
+++ Exercicios/4_Variaveis/js/scripts.js
@@ -0,0 +1,31 @@
+
+var nome = 'João';
+
+var teste = 1;
+console.log(teste);
+
+teste = 'Mateus';
+console.log(teste);
+
+console.log(nome);
+
+var $nome = 'asd';
+var _nome = 'ASD';
+
+console.log($nome);
+console.log(_nome);
+
+var meuPrimeiroNome = "Mateus";
+console.log(meuPrimeiroNome);
+
+let testando = 1;
+const ola = 2;
+
+console.log(testando);
+console.log(ola);
+
+var meuNome;
+console.log(meuNome);
+
+meuNome = "Mateus";
+console.log(meuNome);
