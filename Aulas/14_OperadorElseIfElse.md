e3f0de1a9025548e70edf8b527f6561071d5c7f8
Diego Jefferson <diegojfsr@gmail.com>
2024-02-22 12:37:21 -0300

    14_OperadorElseIfElse

---
+++ Aulas/14_OperadorElseIfElse.md
@@ -0,0 +1,10 @@
+
+Operadore Condicional: Else if e else
+
+- Caso a instrução de if seja negativa, podemos adicionar else.
+- Outra condicional será execultada.
+- Podemos criar bifurcação no codigo.
+- O else if  tem possibilidade d efazer outra verificação.
+- Podemos adicionar outro bloco d ecodigo.
+- Vamos ver na prática!
+
---
+++ Exercicios/13_OperadorElseIfElse/js/scripts.js
@@ -0,0 +1,41 @@
+
+// Operador Condicional: Else if e else
+
+
+var nome = "Mateus";
+
+if(nome=="Pedro"){
+    console.log("O nome dele é Pedro.");
+}else{
+    console.log("O nome dele NÃO é Pedro.");
+}
+
+
+var nome = "Luiz";
+
+if(nome=="Pedro"){
+    console.log("O nome dele é Pedro.");
+}else if(nome=="Mateus"){
+    console.log("O nome dele é Mateus.");
+}else if(nome=="Xavier"){
+    console.log("O nome dele é Xavier.");
+}else{
+    console.log("Ele possui outro nome.");
+}
+
+
+var idade = 19;
+
+if(idade > 20){
+    console.log("Ele pode entrar na festa!");
+}else if(idade >= 18){
+    console.log("Ele só pode entrar com autorização!");
+}
+
+var nome = "Luiz";
+if(nome == "Mateus"){
+    console.log("Teste")
+}else{
+    console.log("Testando")
+}
+
