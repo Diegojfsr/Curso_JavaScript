fe138e79823ecbb92daf11ffe753ea4d6c6dcee7
Diego Jefferson <diegojfsr@gmail.com>
2024-02-22 12:56:04 -0300

    15_ComparadorTipoValor

---
+++ Aulas/15_ComparadorTipoValor.md
@@ -0,0 +1,8 @@
+
+Comparação de valor e tipo de dado e Valor.
+
+- Dois outros operadores de comparação existentes no JavaScript são: === e !==.
+- A função é comparar valor e tipo.
+- Vamos ver na prática!
+
+
---
+++ Exercicios/14_ComparadorTipoValor/js/scripts.js
@@ -0,0 +1,19 @@
+
+// Comparação de valor e tipo de dado
+
+var numero = 5;
+if(numero === 5 ){
+    console.log(" O numero é 5. ");
+}
+
+var numero2 = '7';
+if(numero2 === '7' ){
+    console.log(" O numero é 7. ");
+}
+
+
+var numero3 = 4;
+if(numero3 !== 5 ){
+    console.log(" Não é o numro 5 do tipo Number. ");
+}
+
