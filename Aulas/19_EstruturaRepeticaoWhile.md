c13608eb3c0f1f4cc82e74ed06f07f88fc77a292
Diego Jefferson <diegojfsr@gmail.com>
2024-02-22 17:56:58 -0300

    19_EstruturaRepeticaoWhile

---
+++ Aulas/19_EstruturaRepeticaoWhile.md
@@ -0,0 +1,11 @@
+
+Estruturas de Repetição
+
+- Essas estruturas servem para repetir n vezes uma operação.
+- Exemplo: Repetir uma determinada lógica em cada elemento do array.
+- As estruturas mais comuns são: while e for.
+- Observação: Tomar cuidado com o loop infinito.
+
+
+Estrutura de repetição while
+
---
+++ Exercicios/18_EstruturaRepeticaoWhile/js/scripts.js
@@ -0,0 +1,27 @@
+
+// Estrutura de repetição while
+
+var x = 0;
+while(x < 5){
+    console.log("Testando repetição " + x);
+    x++; // Incrementador
+}
+
+var arr = ["teste1", "teste2", "a", "b"];
+var y = 0;
+while(y <= 3){
+    console.log(arr[y]);
+    y++;
+}
+
+
+var palavra = "Mateus";
+var i = 0;
+while(i <= 6){
+    console.log(palavra[i]);
+    i++;
+}
+
+
+
+
