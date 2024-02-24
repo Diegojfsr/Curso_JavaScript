9e5444dc710afc398e065bf8a01d7bc4b6b42276
Diego Jefferson <diegojfsr@gmail.com>
2024-02-24 17:18:16 -0300

    27_MetodosNumericos

---
+++ Aulas/27_MetodosNumericos.md
@@ -0,0 +1,8 @@
+
+Métodos numéricos (parseInt, parseFloat)
+
+- O objeto Number, pai dos números, contém métodos muito úteis para trabalhar com números em JavaScript.
+- A maioria dos tipos de dados também tem um objeto pai, como : String, Object e Array.
+- Vamos ver na prática!
+
+
---
+++ Exercicios/26_MetodosNumericos/js/scripts.js
@@ -0,0 +1,34 @@
+
+// Métodos numéricos (parseInt, parseFloat)
+
+// ParseFloat 10,0
+
+console.log(parseFloat("12.999"));
+console.log(Number.parseFloat("12.999"));
+
+
+// ParseInt
+
+console.log(parseInt("10"));
+console.log(parseInt("10.96"));
+
+
+// toFixed
+
+console.log(23.515165.toFixed(1));
+
+// isNaN
+
+console.log(isNaN("Teste"));
+console.log(isNaN(12));
+console.log(isNaN("12"));
+
+// MAX_VALUE e MIN_VALUE
+
+console.log(Number.MAX_VALUE);
+console.log(Number.MIN_VALUE);
+
+console.log(2.7976931348623157e+308); //infinity
+
+
+
