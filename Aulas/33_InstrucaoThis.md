d505e570f8643036afbef7939ddcf907a1010c66
Diego Jefferson <diegojfsr@gmail.com>
2024-02-25 16:44:06 -0300

    33_InstrucaoThis

---
+++ Aulas/33_InstrucaoThis.md
@@ -0,0 +1,10 @@
+
+Utilizando o this
+
+- Fora de escopos locais o this sempre se refere ao objeto global Window.
+- Em objetos o this vai ser referir a instância e pode acessar suas propriedades.
+- Vamos ver na prática!
+
+
+
+
---
+++ Exercicios/33_InstrucaoThis/js/scripts.js
@@ -0,0 +1,30 @@
+
+// Utilizando o this
+
+console.log(this);
+
+let pessoa = {
+    nome: "Mateus",
+    idade: 29,
+    falar: function(){
+        console.log("Olá, tudo bem?");
+    },
+    dizerNome: function(){
+        console.log("O meu nome é "+ this.nome);
+    },
+    aniversario: function(){
+        this.idade += 1;
+    },
+    saudacao: function(){
+        return "Sr. " + this.nome;
+    }
+};
+pessoa.dizerNome();
+console.log(pessoa.idade);
+pessoa.aniversario();
+console.log(pessoa.idade);
+
+var sdc = pessoa.saudacao();
+console.log("Olá " + sdc);
+
+
