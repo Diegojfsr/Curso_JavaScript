d9eb5e1d0f12d88cdeadf817b6a1bc489f931f23
Diego Jefferson <diegojfsr@gmail.com>
2024-02-25 17:57:06 -0300

    36_QuerySelector

---
+++ Aulas/36_QuerySelector.md
@@ -0,0 +1,12 @@
+
+querySelector e querySelectorAll
+
+Outras formas d eacessar o DOM
+
+- Com a evolucao das linguagens foram criados dois seletores que acabaram com toda a complexidade desta acao.
+- querySelector e querySelectorAll.
+- Com eles podemos acessar os elementos baseados em regras de Css.
+- Vamos ver na prática!
+
+
+
---
+++ Exercicios/36_QuerySelector/js/scripts.js
@@ -0,0 +1,26 @@
+
+// querySelector e querySelectorAll
+var itensClasse = document.getElementsByClassName("item");
+console.log(itensClasse);
+
+
+// querySelectorAll - pega varios elementos
+var itensQuery = document.querySelectorAll("#lista2 li");
+console.log(itensQuery);
+
+var itensQuery2 = document.querySelectorAll("#lista .item");
+console.log(itensQuery2);
+
+//querySelector - pega um elemento
+var lista = document.querySelector("#lista");
+console.log(lista);
+
+var primeiraLista = document.querySelector("ul");
+console.log(primeiraLista);
+
+var span = document.querySelector("#paragrafo span");
+console.log(span);
+var span = document.querySelectorAll("#paragrafo span")[0];
+console.log(span);
+
+
