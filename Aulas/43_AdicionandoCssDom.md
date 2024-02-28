eaf4a61dbd9bc57e75104d6e725ecc55adef12f5
Diego Jefferson <diegojfsr@gmail.com>
2024-02-28 20:44:49 -0300

    43_AdicionandoCssDom

---
+++ Aulas/43_AdicionandoCssDom.md
@@ -0,0 +1,10 @@
+
+
+Adicionando CSS pelo DOM
+
+Alterando o CSS com JS
+
+- É muito comum alterar CSS com JavaScript também.
+- As regras de CSS ficam de forma inline.
+- Substituindo a maioria das outras regras criadas.
+- Vamos ver na pratica.
---
+++ Exercicios/43_AdicionandoCssDom/js/scripts.js
@@ -0,0 +1,29 @@
+
+
+// Adicionando CSS pelo DOM
+
+// seleciona o elemento
+var title = document.querySelector("#title");
+
+// adiciona o estilo
+title.style.color = "red";
+
+// background color
+
+title.style.backgroundColor = "yellow";
+
+// slecionar elemento
+var subtitle = document.querySelector(".subtitle");
+
+// adicionar varios elementos 
+subtitle.style.cssText = "color: blue; background-color:pink; font-size: 50px";
+
+
+
+
+
+
+
+
+
+
